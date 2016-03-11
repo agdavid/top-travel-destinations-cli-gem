a91da9a7e48814c7b0e22c2d85e6f89d76211d0d
agdavid <antonio.g.david@gmail.com>
2016-03-11 08:14:04 -0500

    outline index-page scraper method

--- top_travel_destinations/lib/top_travel_destinations/scraper.rb
+++ top_travel_destinations/lib/top_travel_destinations/scraper.rb
@@ -1,6 +1,16 @@
 class TopTravelDestinations::Scraper
 
-#1. Need a scraper to get the Top 9 Regions and return an array of hashes to send to Region Class
+  #1. Need a scraper to get the Top 9 Regions and return an array of hashes to send to Region Class
+  def self.scrape_regions_array(index_url)
+    # a. method takes index_url(http://www.fodors.com/world/) as argument
+    # b. uses open-uri to get HTML
+    # c. uses Nokogiri to get NodeNest
+    # e. uses Nokogiri .css method to get element/classes with desired regions
+    # f. iterates to obtain text of regions
+    # g. returns array of hashes
+  end
+
+
 
 #2. Need a scraper to get the Top 10 Destinations of each Region as an array of arrays then send to Region Class and add as key-value pair
 
